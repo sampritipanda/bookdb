1b4ed0e527da7c4a2e4c2da5f260d33ad067c422
Sampriti Panda <sampdingo@hotmail.com>
2014-02-12 23:38:38 -0500

    Rails App Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookdb::Application.config.secret_key_base = '80453b5d8f1b7df6c409a189719d328cb16156fbdb898967f265d378abd0a253f22ec0777e46a289f8928b2b5a14320373220cdbb8628dce8557db163c67190c'
